b2a0bdd8f63a4b15d0c509ca3314871cd6472848
Rick Chen <rick20001010@gmail.com>
2023-05-12 17:57:17 +0000

    20230513

---
+++ app.py
@@ -0,0 +1,22 @@
+# Rick add person class 5/13
+import random
+class Person:
+    def __init__(self,Name,Video_Count,Character):
+        self.Name = Name
+        self.Video_Count = Video_Count
+        self.Character = Character
+    def Rent():
+        pass
+    def Return():
+        pass
+
+# create customer
+Name_list = ['Emma','Liam','Olivia','Noah','Ava','Sophia','Jackson','Isabella','Aiden','Mia']
+Character_list = ['Breezy','Hoarders','Regular']
+Customer_list = [0 for i in range(10)]
+for i in range(10):
+    rd = random.randint(0, 2)
+    Customer_list[i] =Person(Name_list[i],0,Character_list[rd])
+    print(Customer_list[i].Name,Customer_list[i].Video_Count,Customer_list[i].Character)
+
+
