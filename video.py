7f32d2d9556e3abcb0ae71641ea87b6ae6e0b1a9
劉’s咩噗囉 <howard@liuhaochendeMacBook-Pro.local>
2023-05-13 23:21:34 +0800

    create video instance

--- video.py
+++ video.py
@@ -3,32 +3,41 @@ class Video(abc.ABC):
     pass
 
 class Romance(Video):
-    def __init__(self, videoName, price, status, startTime):
+    def __init__(self, videoName, status, startTime):
         self.videoName = videoName
         self.price = 2 # undetermined
         self.status = status
         self.startTime = startTime
     
 class NewRelease(Video):
-    def __init__(self, videoName, price, status, startTime):
+    def __init__(self, videoName, status, startTime):
         self.videoName = videoName
         self.price = 5 # undetermined
         self.status = status
         self.startTime = startTime
 
 class Comedy(Video):
-    def __init__(self, videoName, price, status, startTime):
+    def __init__(self, videoName, status, startTime):
         self.videoName = videoName
         self.price = 2 # undetermined
         self.status = status
         self.startTime = startTime
     
 class Horror(Video):
-    def __init__(self, videoName, price, status, startTime):
+    def __init__(self, videoName, status, startTime):
         self.videoName = videoName
         self.price = 4 # undetermined
         self.status = status
         self.startTime = startTime
     
-a = Horror('1',  4, 'Home', '0512')
+a = Horror('1', 'Home', '0512')
 print(a.videoName)
+
+videoList = []
+videoKind = ['Romance', 'NewRelease', 'Comedy', 'Horror']
+for i in videoKind:
+    for _ in range(1, 6):
+        locals()[i+str(_) ] = Romance(f'{i}{_}', 'Onboard', None)
+        videoList.append(f'{i}{_}')
+
+print(videoList)
