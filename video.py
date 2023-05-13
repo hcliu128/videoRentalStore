bb96592acfef2b12a80abf7fa9430a5a8fa07a3e
劉’s咩噗囉 <howard@liuhaochendeMacBook-Pro.local>
2023-05-13 22:04:17 +0800

    Video created

---
+++ video.py
@@ -0,0 +1,32 @@
+from abc import ABCMeta
+class Video(ABCMeta):
+    def __init__(self):
+        pass
+
+class Romance(Video):
+    def __init__(self, videoName, price, status, startTime):
+        self.videoName = videoName
+        self.price = 2 # undetermined
+        self.status = status
+        self.startTime = startTime
+    
+class NewRelease(Video):
+    def __init__(self, videoName, price, status, startTime):
+        self.videoName = videoName
+        self.price = 5 # undetermined
+        self.status = status
+        self.startTime = startTime
+
+class Comedy(Video):
+    def __init__(self, videoName, price, status, startTime):
+        self.videoName = videoName
+        self.price = 2 # undetermined
+        self.status = status
+        self.startTime = startTime
+    
+class Horror(Video):
+    def __init__(self, videoName, price, status, startTime):
+        self.videoName = videoName
+        self.price = 4 # undetermined
+        self.status = status
+        self.startTime = startTime
