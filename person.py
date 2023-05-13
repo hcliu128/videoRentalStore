8d7440351fb69c7f5f4745f8642b5864c91f4ef2
Rick Chen <rick20001010@gmail.com>
2023-05-13 14:16:09 +0000

    20230513

--- person.py
+++
@@ -1,9 +0,0 @@
-class Person:
-    def __init__(self,Name,Video_Count,Character):
-        self.Name = Name
-        self.Video_Count = Video_Count
-        self.Character = Character
-    def Rent():
-        pass
-    def Return():
-        pass
