6052f7badd2e5449de48997cddcb43c50668ab27
Rick Chen <rick20001010@gmail.com>
2023-05-14 18:07:26 +0000

    20230515

--- main.py
+++ main.py
@@ -19,32 +19,36 @@ class Main:
         for i in range(10):
             rd = random.randint(0, 2)
             self.Customer_list[i] =person.Person(self.Name_list[i],0,self.Character_list[rd])
+            self.store.add_customer(self.Customer_list)
     def show_customer(self):
-        for i in range(10):
-            print(self.Customer_list[i].Name,self.Customer_list[i].Video_Count,self.Customer_list[i].Character)
+        self.store.show_customer()
     def set_videos(self):
         for i in range(5):
             for j in range(4):
                 if i==0:
-                    self.Video[i][j]=video.Romance(self.Romance_video[j], True, datetime.date.today())
+                    self.Video[i][j]=video.Romance(self.Romance_video[j], 'Onboard', 0,'null')
                 if i==1:
-                    self.Video[i][j]=video.Comedy(self.Comedy_video[j], True, datetime.date.today())
+                    self.Video[i][j]=video.Comedy(self.Comedy_video[j], 'Onboard', 0,'null')
                 if i==2:
-                    self.Video[i][j]=video.Horror(self.Horror_video[j], True, datetime.date.today())
+                    self.Video[i][j]=video.Horror(self.Horror_video[j], 'Onboard', 0,'null')
                 if i==3:
-                    self.Video[i][j]=video.Drama(self.Drama_video[j], True, datetime.date.today())
+                    self.Video[i][j]=video.Drama(self.Drama_video[j], 'Onboard', 0,'null')
                 if i==4:
-                    self.Video[i][j]=video.NewRelease(self.New_release_video[j], True, datetime.date.today())
+                    self.Video[i][j]=video.NewRelease(self.New_release_video[j], 'Onboard', 0,'null')
         self.store.add_video(self.Video)
     def show_video(self):
         self.store.show_video()
-    def run():
-        for i in range(34):
-            pass
-
+    def run(self,day):
+        n = random.randint(0,9)
+        video_type = random.randint(0,4)
+        video_slot = random.randint(0,3)
+        self.store.lendVideo(person = self.store.Customer_list[n],video = self.store.videos_list[video_type][video_slot],startTime = day)
+        self.store.returnVideo(video = self.store.videos_list[video_type][video_slot], person = self.store.Customer_list[n], endTime = day+3, price = self.store.videos_list[video_type][video_slot].price)
 main = Main()
 main.set_customer()
-main.show_customer()
+#main.show_customer()
 main.set_videos()
-main.show_video()
+#main.show_video()
+for i in range(35):
+    main.run(i)
 
--- videostore.py
+++ videostore.py
@@ -1,21 +1,39 @@
 class VideoStore:
     def __init__(self):
-        self.videos = [[0 for j in range(4)] for i in range(5)]
+        self.videos_list = [[0 for j in range(4)] for i in range(5)]
         self.dailyRecord = []
-        self.Customer_list = []
+        self.Customer_list =[]
         self.videoInventory = 20
     def add_video(self,video):
-        self.videos = video
-    def add_customer(self):
-        pass
+        self.videos_list = video
+    def add_customer(self,Person):
+        self.Customer_list = Person
+    def show_customer(self):
+        for i in range(10):
+            print(self.Customer_list[i].Name,self.Customer_list[i].Video_Count,self.Customer_list[i].Character)
     def show_video(self):
         #print(self.videos)
         for i in range(5):
             for j in range(4):
-                print(self.videos[i][j].videoName,self.videos[i][j].status,self.videos[i][j].startTime)
-    def lend(self):
-        pass
-    def Return(self):
-        pass
+                print(self.videos_list[i][j].videoName,self.videos_list[i][j].status,self.videos_list[i][j].startTime,self.videos_list[i][j].endTime)
+    def lendVideo(self, **kwargs):
+        (person, video, startTime) = (kwargs['person'], kwargs['video'], kwargs['startTime'])
+        video.status = person.Name
+        video.startTime = startTime
+        person.Video_Count += 1
+        print('|success lend','|video='+video.videoName,'\t|status='+video.status,'\t|start time='+str(video.startTime),'|count='+str(person.Video_Count))
+    def returnVideo(self, **kwargs):
+        data = {}
+        (person, video, endTime, price) = (kwargs['person'], kwargs['video'], kwargs['endTime'], kwargs['price'])
+        video.status = 'Onboard'
+        startTime = video.startTime
+        video.endTime = endTime
+        person.Video_Count -= 1
+        data['person'] = person.Name
+        data['video'] = video.videoName
+        data['rental_day'] = endTime-startTime
+        data['cost'] = (endTime-startTime) * price
+        self.dailyRecord.append(data)
+        print(data)
     def Daily_record(self):
         pass        
