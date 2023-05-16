fa384f257eb9a73d09df416351522478660e9bc6
Howard Liu <vv24825051@gmail.com>
2023-05-17 01:29:57 +0800

    517

--- video.py
+++ video.py
@@ -3,40 +3,40 @@ class Video(abc.ABC):
     pass
 
 class Romance(Video):
-    def __init__(self, videoName, status, startTime, duration):
-        self.videoName = videoName
-        self.price = 2 # undetermined
+    def __init__(self, video_name, status, start_time, duration):
+        self.video_name = video_name
+        self.price = 2 
         self.status = status
-        self.startTime = startTime
+        self.start_time = start_time
         self.duration = duration
     
 class NewRelease(Video):
-    def __init__(self, videoName, status, startTime, duration):
-        self.videoName = videoName
-        self.price = 5 # undetermined
+    def __init__(self, video_name, status, start_time, duration):
+        self.video_name = video_name
+        self.price = 5 
         self.status = status
-        self.startTime = startTime
+        self.start_time = start_time
         self.duration = duration
 
 class Comedy(Video):
-    def __init__(self, videoName, status, startTime, duration):
-        self.videoName = videoName
-        self.price = 2 # undetermined
+    def __init__(self, video_name, status, start_time, duration):
+        self.video_name = video_name
+        self.price = 2 
         self.status = status
-        self.startTime = startTime
+        self.start_time = start_time
         self.duration = duration
     
 class Horror(Video):
-    def __init__(self, videoName, status, startTime, duration):
-        self.videoName = videoName
-        self.price = 4 # undetermined
+    def __init__(self, video_name, status, start_time, duration):
+        self.video_name = video_name
+        self.price = 4 
         self.status = status
-        self.startTime = startTime
+        self.start_time = start_time
         self.duration = duration
 class Drama(Video):
-    def __init__(self, videoName, status, startTime,duration):
-        self.videoName = videoName
-        self.price = 4 # undetermined
+    def __init__(self, video_name, status, start_time,duration):
+        self.video_name = video_name
+        self.price = 4 
         self.status = status
-        self.startTime = startTime
+        self.start_time = start_time
         self.duration = duration
